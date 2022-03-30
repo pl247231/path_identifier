3b0f187db87b5a3d91f89669b62b72c7db0f9575
pl247231 <70585668+pl247231@users.noreply.github.com>
2022-03-30 11:45:00 -0400

    Update curve.py

--- curve.py
+++ curve.py
@@ -3,16 +3,35 @@ import numpy as np
 
 img = cv.imread("/Users/local/PycharmProjects/curve/IMG_4812.jpg")
 grayed = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-width = img.shape[0]
-height = img.shape[1]
-for y in range(height-1):
-    check = False
-    startx = 0
-    for x in range(width-1):
-        if (grayed[x][y] < 100 and check == False):
+height = img.shape[0]-1
+width = img.shape[1]-1
+slice_height = height//50
+slice_width = width//50
+midpoints = []
+for j in range(50):
+    y = height-j*slice_height
+    check1 = False
+    check2 = False
+    for x in range(width):
+        if(grayed[y,x] < 50):
             startx = x
-        elif(grayed[x][y] < 100 and check == True):
-            cv.line(grayed, pt1=(startx, y), pt2=(x, y), color=0, thickness=3)
-cv.imshow('hi', grayed)
+            check1 = True
+            break
+    for x in range(width):
+        if grayed[y,width-x] < 20:
+            endx = width-x
+            check2 = True
+            break
+    if(check1 and check2):
+        mid = (startx+endx)//2
+        midpoints.append((mid,y))
+    else:
+        ending = y
+        break
+
+cv.line(img, pt1 = (startx,1000), pt2 = (endx,1000), color = (0,0,0), thickness= 10)
+
+
+cv.imshow('hi', img)
 cv.waitKey()
 
