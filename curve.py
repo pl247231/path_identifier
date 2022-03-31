bddc9428fad9936f41927785a47f3599437c86d2
pl247231 <70585668+pl247231@users.noreply.github.com>
2022-03-31 11:43:51 -0400

    Update curve.py

--- curve.py
+++ curve.py
@@ -1,19 +1,21 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread("/Users/local/PycharmProjects/curve/IMG_4812.jpg")
+img = cv.imread("/Users/local/PycharmProjects/curve/IMG-4812.jpg")
 grayed = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 height = img.shape[0]-1
 width = img.shape[1]-1
-slice_height = height//50
-slice_width = width//50
+slice_height = height//20
+
 midpoints = []
-for j in range(50):
+
+for j in range(20):
     y = height-j*slice_height
     check1 = False
     check2 = False
+
     for x in range(width):
-        if(grayed[y,x] < 50):
+        if(grayed[y,x] < 15):
             startx = x
             check1 = True
             break
@@ -29,7 +31,27 @@ for j in range(50):
         ending = y
         break
 
-cv.line(img, pt1 = (startx,1000), pt2 = (endx,1000), color = (0,0,0), thickness= 10)
+slice_width = (width - midpoints[-1][0])//15
+second_x = midpoints[-1][0]
+for i in range(14):
+    x = second_x + slice_width*(i+1)
+    check1 = False
+    check2 = False
+    for y in range(height):
+        if(grayed[y,x] < 50):
+            starty = y
+            check1 = True
+            break
+    for y in range(height):
+        if(grayed[height-y,x] < 50):
+            endy = height-y
+            check2 = True
+            break
+    if (check1 and check2):
+        mid = (starty + endy) // 2
+        midpoints.append((x, mid))
+for i in range(len(midpoints) - 1):
+    cv.arrowedLine(img = img, pt1 = midpoints[i], pt2 = midpoints[i+1], thickness = 5, color = (0,255 ,0))
 
 
 cv.imshow('hi', img)
